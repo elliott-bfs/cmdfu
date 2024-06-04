93e26d49f3694b571fe2d8d90b3cc7dbfacec544
Lars Häring <lars.haring@microchip.com>
2024-06-04 13:55:40 +0200

    Remove obsolete tests.

--- test/test/test_mdfu_packet.c
+++ test/test/test_mdfu_packet.c
@@ -26,19 +26,16 @@ void test_mdfu_packet(void){
         .data_length = 4,
         .data = (uint8_t *) &cmd_packet[2]
     };
+    mdfu_packet_t decoded_packet;
 
     // Log a packet
     mdfu_log_packet(&packet, MDFU_CMD);
 
-    mdfu_encode_cmd_packet_cp(&packet,(uint8_t *) &packet_buffer.buffer, &packet_buffer.size);
+    mdfu_encode_cmd_packet(&packet,(uint8_t *) &packet_buffer.buffer, &packet_buffer.size);
     TEST_ASSERT_EQUAL(packet.data_length, 4);
     TEST_ASSERT_EQUAL_UINT8_ARRAY(cmd_packet, packet_buffer.buffer, 6);
+    mdfu_decode_packet(&decoded_packet, MDFU_CMD, (uint8_t *) &packet_buffer.buffer, packet_buffer.size);
 
-    // Encode a packet, then docode again and compare with original data
-    mdfu_encode_cmd_packet(&packet, &packet_buffer);
-    mdfu_packet_t decoded_packet;
-    mdfu_decode_packet(&decoded_packet, MDFU_CMD, &packet_buffer);
-    
     TEST_ASSERT_EQUAL_INT8(packet.sequence_number, decoded_packet.sequence_number);
     TEST_ASSERT_EQUAL_INT8(packet.command, decoded_packet.command);
     TEST_ASSERT_EQUAL(packet.sync, decoded_packet.sync);
