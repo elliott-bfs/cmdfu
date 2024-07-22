5a4808905313d9d9d3b6aa6a072c1a6ce24251ee
Lars Häring <lars.haring@microchip.com>
2024-07-22 15:00:17 +0200

    Clean up serial transport and add clean error messaging.

--- src/transport/serial_transport.c
+++ src/transport/serial_transport.c
@@ -1,25 +1,88 @@
+/**
+ * @file serial_transport.c
+ * @brief Serial transport layer.
+ */
 #include <stdint.h>
 #include <stdbool.h>
-#include <stdio.h> // printf
+#include <stdio.h>
 #include <errno.h>
 #include <assert.h>
 #include "mdfu/serial_transport.h"
 #include "mdfu/timeout.h"
 #include "mdfu/logging.h"
+#include "mdfu_config.h"
+#include "mdfu/mdfu.h"
 
+/** @def FRAME_CHECK_SEQUENCE_SIZE
+ *  @brief Size of the frame check sequence in bytes.
+ */
 #define FRAME_CHECK_SEQUENCE_SIZE 2
+
+/** @def FRAME_PAYLOAD_LENGTH_SIZE
+ *  @brief Size of payload length field in bytes.
+ */
 #define FRAME_PAYLOAD_LENGTH_SIZE 2
+
+/** @def FRAME_START_CODE_SIZE
+ *  @brief Size of the frame start code in bytes.
+ */
+#define FRAME_START_CODE_SIZE 1
+
+/** @def FRAME_END_CODE_SIZE
+ *  @brief Size of the frame end code in bytes.
+ */
+#define FRAME_END_CODE_SIZE 1
+
+/** @def FRAME_START_CODE
+ *  @brief Indicates the start of a frame.
+ */
 #define FRAME_START_CODE 0x56
+
+/** @def FRAME_END_CODE
+ *  @brief The ending byte code of a frame.
+ */
 #define FRAME_END_CODE 0x9E
+
+/** @def ESCAPE_SEQ_CODE
+ *  @brief The byte code used to indicate the start of an escape sequence.
+ */
 #define ESCAPE_SEQ_CODE 0xCC
+
+/** @def FRAME_START_ESC_SEQ
+ *  @brief The escape sequence replacement for the frame start code.
+ */
 #define FRAME_START_ESC_SEQ (~FRAME_START_CODE & 0xff)
+
+/** @def FRAME_END_ESC_SEQ
+ *  @brief The escape sequence replacement for the frame end code.
+ */
 #define FRAME_END_ESC_SEQ  (~FRAME_END_CODE & 0xff)
+
+/** @def ESCAPE_SEQ_ESC_SEQ
+ *  @brief The escape sequence replacement for the escape sequence code itself.
+ */
 #define ESCAPE_SEQ_ESC_SEQ (~ESCAPE_SEQ_CODE & 0xff)
 
 
+/** 
+ * @brief MAC layer interface used for transport layer communication.
+ * 
+ * This variable holds the MAC layer interface.
+ */
 static mac_t transport_mac;
-static int transport_timeout;
-static uint8_t buffer[1024];
+
+/** 
+ * @brief Buffer for storing data frames.
+ * 
+ * This buffer is used to store the data frames that are constructed or received
+ * by the transport layer. The size of the buffer is calculated based on the
+ * maximum expected size of the frames, including start/end codes, sequence field,
+ * command size, maximum command data length, and frame check sequence.
+ * 
+ * @note The buffer size is calculated for the worst case scenario where all data
+ * bytes consist of reserved codes that need to be replaced with escape sequences.
+ */
+static uint8_t buffer[FRAME_START_CODE + (MDFU_SEQUENCE_FIELD_SIZE + MDFU_COMMAND_SIZE  + MDFU_MAX_COMMAND_DATA_LENGTH + FRAME_CHECK_SEQUENCE_SIZE) * 2 + FRAME_END_CODE_SIZE];
 
 /**
  * @brief Calculate inverted 16-bit two's complement frame checksum.
@@ -143,7 +206,27 @@ ssize_t read_until(uint8_t code, int max_size, uint8_t *data, timeout_t timer){
     return status;
 }
 
-ssize_t read_and_decode_until(uint8_t code, int max_size, uint8_t *data, timeout_t timer){
+/**
+ * @brief Reads data from a transport layer and decodes it until the frame end code is received.
+ *
+ * This function reads bytes one by one from a transport layer, handling escape sequences
+ * and stopping when the frame end code is encountered or the buffer is full. If an escape
+ * sequence is detected, the function decodes it to the original byte. The function also
+ * checks for a timeout condition.
+ *
+ * @param max_size The maximum number of bytes to read into the buffer.
+ * @param data A pointer to the buffer where the decoded data will be stored.
+ * @param timer A timeout_t structure that defines the timeout condition.
+ *
+ * @return On success, the number of bytes read and decoded. On failure, -1 and errno is set
+ *         to indicate the error (ENOBUFS for buffer overflow, EINVAL for invalid escape sequence,
+ *         ETIMEDOUT for timeout).
+ *
+ * @warning The function asserts that the status returned by transport_mac.read is less than or equal to 1.
+ *          The transport_mac.read function is assumed to be a part of the transport_mac object which should
+ *          be defined and initialized elsewhere.
+ */
+ssize_t read_and_decode_until(int max_size, uint8_t *data, timeout_t timer){
     int status = -1;
     uint8_t tmp;
     uint8_t *pdata = data;
@@ -167,13 +250,13 @@ ssize_t read_and_decode_until(uint8_t code, int max_size, uint8_t *data, timeout
                 break;
             } 
             if(escape_code){
+                escape_code = false;
                 if(tmp == FRAME_START_ESC_SEQ){
-                    *pdata = FRAME_START_CODE;
+                    *pdata++ = FRAME_START_CODE;
                 } else if(tmp == FRAME_END_ESC_SEQ){
-                    *pdata = FRAME_END_CODE;
+                    *pdata++ = FRAME_END_CODE;
                 } else if(tmp == ESCAPE_SEQ_ESC_SEQ){
-                    *pdata = ESCAPE_SEQ_CODE;
-                pdata++;
+                    *pdata++ = ESCAPE_SEQ_CODE;
                 } else {
                     DEBUG("Invalid code (%x) after escape code", tmp);
                     status = -1;
@@ -181,7 +264,7 @@ ssize_t read_and_decode_until(uint8_t code, int max_size, uint8_t *data, timeout
                     break;
                 }
             } else {
-                if(code == ESCAPE_SEQ_CODE){
+                if(tmp == ESCAPE_SEQ_CODE){
                     escape_code = true;
                 } else {
                     *pdata = tmp;
@@ -206,7 +289,7 @@ ssize_t read_and_decode_until(uint8_t code, int max_size, uint8_t *data, timeout
  * into the global transport_mac structure and sets the transport timeout.
  *
  * @param mac Pointer to a mac_t structure that contains the MAC layer function pointers.
- * @param timeout The timeout value to be used for transport layer operations.
+ * @param timeout The default timeout value to be used for transport layer operations.
  * @return Always returns 0 to indicate success.
  */
 static int init(mac_t *mac, int timeout){
@@ -215,18 +298,52 @@ static int init(mac_t *mac, int timeout){
     transport_mac.read = mac->read;
     transport_mac.write = mac->write;
     transport_mac.init = mac->init;
-    transport_timeout = timeout;
     return 0;
 }
 
+/**
+ * @brief Opens the transport layer.
+ *
+ * This function calls the open method on the transport_mac object, which is responsible
+ * for establishing the transport layer connection.
+ *
+ * @return int Returns 0 on success, or a non-zero error code on failure.
+ */
 static int open(void){
     return transport_mac.open();
 }
 
+/**
+ * @brief Closes the transport layer.
+ *
+ * This function calls the close method on the transport_mac object, which is responsible
+ * for closing the transport layer connection.
+ *
+ * @return int Returns 0 on success, or a non-zero error code on failure.
+ */
 static int close(void){
     return transport_mac.close();
 }
 
+/**
+ * @brief Encodes the payload of a frame by escaping special codes.
+ *
+ * This function takes an array of data and encodes it by inserting escape sequences
+ * before any special codes that appear in the data. The special codes include the
+ * start of frame code, end of frame code, and the escape sequence code itself.
+ *
+ * @param data_size The size of the input data array.
+ * @param data A pointer to the array of data that needs to be encoded.
+ * @param encoded_data A pointer to the array where the encoded data will be stored.
+ * @param encoded_data_size A pointer to an integer where the size of the encoded data will be stored.
+ *
+ * @note The encoded_data array should be large enough to accommodate the encoded data,
+ *       which may be larger than the input data due to the addition of escape sequences.
+ *
+ * @warning The function assumes that the encoded_data array has enough space to hold
+ *          the encoded data. If the encoded_data array is not large enough, a buffer
+ *          overflow can occur.
+ */
 static void encode_frame_payload(int data_size, uint8_t *data, uint8_t *encoded_data, int *encoded_data_size){
     uint8_t code;
     int size = 0;
